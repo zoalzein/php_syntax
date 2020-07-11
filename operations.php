c3ac15b24b3f4cfbd9671ad6ca18be513277d811
Zeinab O. Zein <zeinab.omar.zein@hotmail.com>
2020-07-12 00:12:26 +0300

    Assignment operator(=) Addition operator Subtraction operator Multiplication operator Division operator Concatenation operator

--- operations.php
+++ operations.php
@@ -1,3 +1,19 @@
 <?php
-echo "Hello, its me";
-?>
+
+$number1 = 1;
+$number2 = 2;
+echo "$number1 + $number2 = " . ($number1 + 3) . "<br>";
+$firsname = "Zeinab Omar";
+$last_name = "Zein";
+$full_name = $first_name . " " . $last_name;
+echo $full_name . "<br>";
+echo ($number1 + $number2) . "<br>";
+echo ($number1 - $number2) . "<br>";
+echo ($number1 * $number2) . "<br>";
+echo ($number2 / $number1) . "<br>";
+echo ($number1 - $number2 * $number2 + $number1 / $number2) . "<br>";
+$firstname = "Hashim Omar";
+$lastname =  "Zein";
+echo $firstname . " " . $lastname;
+
+  
