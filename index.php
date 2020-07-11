6f267134bc2b7b598e17adaa66b32df0e953ad32
Zeinab O. Zein <zeinab.omar.zein@hotmail.com>
2020-07-12 00:06:29 +0300

    Introduction to HTML links

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
-$number1=1;
-$number2=2;
-echo $number1 + $number2;
-echo "<br>";
-$number2=4;
-echo $number1 + $number2;
-
-
+echo "Learning PHP with ease.";
 ?>
-    
+<a href="operations.php">Operations</a>
+
