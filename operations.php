7ae6cd1aa6617bd803bfb31515287f25dbad5b83
Zeinab O. Zein <zeinab.omar.zein@hotmail.com>
2020-07-14 23:22:40 +0300

    Introduction to comments.(Block & single line) Task regarding modulo operator accomplished.

--- operations.php
+++ operations.php
@@ -1,9 +1,11 @@
+<a href="index.php">Home</a>
+<br>
 <?php
 
 $number1 = 1;
 $number2 = 2;
-echo "$number1 + $number2 = " . ($number1 + 3) . "<br>";
-$firsname = "Zeinab Omar";
+echo "$number1 + $number2 = " . ($number1 + $number2) . "<br>";
+$first_name = "Zeinab Omar";
 $last_name = "Zein";
 $full_name = $first_name . " " . $last_name;
 echo $full_name . "<br>";
@@ -17,4 +19,3 @@ $lastname = "Zein";
 echo $firstname . " " . $lastname . "<br>";
 $number2 = 7;
 echo $number2 % 3;
-
---
+++ tasks.php
@@ -0,0 +1,8 @@
+<a href="index.php">Home</a>
+<br>
+<?php
+
+// 1. Write a logic to calculate the product of the remainders when 17 and 27 are divided by 4
+$number1 = 199;
+$number2 = 8;
+echo "($number1 % 4) X ($number2 % 4) = " . ($number1 % 4) * ($number2 % 4) . "<br>";
