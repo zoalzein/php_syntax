c5e4ee8ebbfdd91d43d1641fbf3177b334a0da8c
Zeinab O. Zein <zeinab.omar.zein@hotmail.com>
2020-07-17 01:04:05 +0300

    Tasks successfully completed.

--- tasks.php
+++ tasks.php
@@ -13,6 +13,7 @@ echo "($number1 % 4) X ($number2 % 4) = " . ($number1 % 4) * ($number2 % 4) . "<
 //d). Compare if the twice the first number is 4 less than the second number
 //e). Compare if the first number is double the half the second number
 //f). Compare if 5 less than the first number is 5 more than double the second number
+
 $number1 = 45;
 $number2 = 8;
 var_dump($number1 == $number2);
@@ -27,3 +28,23 @@ var_dump(( $number1 ) > ( 2 * $number2 / 2));
 echo "<br>";
 var_dump(( $number1 - 5) > ( (2 * $number2) + 5 ));
 echo "<br>";
+
+//3. Declare threee unequal variables and assign them with values.
+//a). use if condition to check if twice the first is 3 less than the second and third
+//b). use if condition to check if the first and the second are 5 or less than twice the third
+//c). use if condition to check if the average of the three is 5 or less than the second and the third
+$number1 = 6;
+$number2 = 8;
+$number3 = 10;
+if (((2 * $number1) == ($number2 - 3)) && ((2 * $number1) == ($number3 - 3))) {
+    echo "number1 = $number1 mumber2 = $number2 number3 = $number3" . "<br>";
+}
+if ((2 * $number3 - $number1 <= 5) && (2 * $number3 - $number2 <= 5)) {
+    echo "number1 = $number1 mumber2 = $number2 number3 = $number3" . "<br>";
+}
+if (($number2 - (($number1 + $number2 + $number3) / 3) <= 5) && ($number3 - (($number1 + $number2 + $number3) / 3) <= 5)) {
+    echo "number1 = $number1 mumber2 = $number2 number3 = $number3" . "<br>";
+} 
+
+        
+        
